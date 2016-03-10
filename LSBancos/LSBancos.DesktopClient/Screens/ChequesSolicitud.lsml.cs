e403c3bcc8ae60decf643f722952cf6175dc4798
laos <luis.osinaga@gmail.com>
2016-03-10 11:58:15 -0400

    Comienzo de día 10/03/2015

---
+++ LSBancos/LSBancos.DesktopClient/Screens/ChequesSolicitud.lsml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class ChequesSolicitud
+    {
+        IContentItemProxy comboCuentas;
+
+        void Bindings()
+        {
+            comboCuentas.SetBinding(System.Windows.Controls.ComboBox.ItemsSourceProperty, 
+                                        "Screen.Bancos", 
+                                        System.Windows.Data.BindingMode.OneWay);
+            comboCuentas.SetBinding(System.Windows.Controls.ComboBox.SelectedItemProperty, 
+                                        "Screen.Bancos.SelectedItem", 
+                                        System.Windows.Data.BindingMode.TwoWay);
+        }
+            
+        void FindControls()
+        {
+            comboCuentas = this.FindControl("ComboBancos");
+        }
+
+
+        partial void ChequesSolicitud_InitializeDataWorkspace(List<IDataService> saveChangesTo)
+        {
+            FindControls();
+            Bindings();
+        }
+    }
+}
