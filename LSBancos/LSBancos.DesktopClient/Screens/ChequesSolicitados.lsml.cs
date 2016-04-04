4273a3d491cf60d4a41d4c265f658213c2d30545
laos <luis.osinaga@gmail.com>
2016-04-04 09:23:14 -0400

    Principio de día 04/04/2016

---
+++ LSBancos/LSBancos.DesktopClient/Screens/ChequesSolicitados.lsml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.LightSwitch.Presentation.Extensions;
+using Microsoft.LightSwitch.Presentation;
+using Microsoft.LightSwitch.Framework.Client;
+using Microsoft.LightSwitch;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class ChequesSolicitados
+    {
+        #region Propiedades
+        #endregion
+
+        #region Metodos Auxiliares
+        #endregion
+
+        #region Metodos Autogenerados
+
+        partial void ChequesSolicitados_InitializeDataWorkspace(List<IDataService> saveChangesTo)
+        {
+            // Write your code here.
+
+        }
+
+        #endregion
+    }
+}
