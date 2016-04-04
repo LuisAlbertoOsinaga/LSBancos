50f24b124211fe0ec5f20f3b673a3a71e15f1e3b
laos <luis.osinaga@gmail.com>
2016-04-04 14:22:23 -0400

    Intermedio 04/04/2016

--- LSBancos/LSBancos.DesktopClient/Screens/ChequesSolicitados.lsml.cs
+++ LSBancos/LSBancos.DesktopClient/Screens/ChequesSolicitados.lsml.cs
@@ -7,6 +7,7 @@ using System.IO.IsolatedStorage;
 using System.IO;
 using System.Linq;
 using System;
+using System.Windows;
 
 namespace LightSwitchApplication
 {
@@ -24,6 +25,63 @@ namespace LightSwitchApplication
         {
             // Write your code here.
 
+        }
+
+        partial void ChequesDeleteSelected_CanExecute(ref bool result)
+        {
+            // Write your code here.
+
+        }
+
+        partial void ChequesDeleteSelected_Execute()
+        {
+            MessageBoxResult result = this.ShowMessageBox(string.Format("Desea eliminar el cheque '{0}' de esta solicitud?",
+                                                                            Cheques.SelectedItem.Nro),
+                                                                        "CONFIRMACION", MessageBoxOption.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                if (Cheques.SelectedItem.Details.EntityState == EntityState.Added)
+                {
+                    Cheques.RemoveSelected();
+                    this.Refresh();
+                }
+                else
+                    Cheques.SelectedItem.Delete();
+            }
+        }
+
+        partial void AnularCheque_Execute()
+        {
+            this.OpenModalWindow("AnulacionCheque");
+        }
+
+        partial void AnulacionCheque_Execute()
+        {
+            if(!FechaAnulacion.HasValue)
+            {
+                this.ShowMessageBox("Introduzca fecha de Anulación!");
+                return;
+            }
+            if (string.IsNullOrEmpty(CausaAnulacion))
+            {
+                this.ShowMessageBox("Introduzca Causa de la Anulación!");
+                return;
+            }
+
+            Cheques.SelectedItem.Estado = "A";  // A-Anulado
+            Cheques.SelectedItem.FechaAnulacion = FechaAnulacion;
+            Cheques.SelectedItem.Observaciones = string.Format("|Anulado por: '{0}'", CausaAnulacion);
+            this.CloseModalWindow("AnulacionCheque");
+        }
+
+        partial void CancelaAnulacion_Execute()
+        {
+            this.CloseModalWindow("AnulacionCheque");
+        }
+
+        partial void ChequesSolicitados_Saved()
+        {
+            this.Refresh();
         }
 
         #endregion
