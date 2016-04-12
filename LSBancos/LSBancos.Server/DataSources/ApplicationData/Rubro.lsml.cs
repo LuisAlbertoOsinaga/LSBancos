1623605bc6d79f3962afc6ea2fd387dc669b4cb6
laos <luis.osinaga@gmail.com>
2016-04-12 17:34:57 -0400

    Final 2016-04-12

---
+++ LSBancos/LSBancos.Server/DataSources/ApplicationData/Rubro.lsml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.LightSwitch;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace LightSwitchApplication
+{
+    public partial class Rubro
+    {
+        partial void Nombre_Changed()
+        {
+            if (!string.IsNullOrEmpty(this.Nombre))
+                this.Nombre = this.Nombre.Trim().ToUpper();
+        }
+    }
+}
